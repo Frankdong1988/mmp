fdfa0e8db5f297f52c05953f27f4abeaf1de90cf
dgf_gitHubTest_innitSubmit <frankdong1988@163.com>
2017-12-08 22:52:49 +0800

    添加类

---
+++ src/main/java/com/frank/mmp/system/bean/MenuPackageBean.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.frank.mmp.system.bean;
+
+import java.util.List;
+
+/**
+ * @author Administrator
+ *
+ */
+public class MenuPackageBean extends MenuBean {
+	private List<MenuPackageBean> list;
+
+	public List<MenuPackageBean> getList() {
+		return list;
+	}
+	public void setList(List<MenuPackageBean> list) {
+		this.list = list;
+	}
+	
+}
